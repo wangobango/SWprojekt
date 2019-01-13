14be94151afbec2b2adf2244d9b3847048fed639
wangobango <puch003@wp.pl>
2019-01-13 13:20:22 +0100

    basic motor control

--- projektSW.ino
+++ projektSW.ino
@@ -3,34 +3,37 @@
 #include <string.h>
 
 const int ledPin = 13;
-int n = 0;
+
 int enablePin = 11;
+int enablePin2 = 6;
 int in1Pin = 10;
 int in2Pin = 9;
-int switchPin = 7;
-int potPin = 0;
-unsigned int resp;
-String data;
-char *buffor[255];
+int int3Pin = 8;
+int int4Pin = 7;
+
 char c;
+int speed = 255;
+boolean reverse = true;
 
 void setup() {
     pinMode(ledPin,OUTPUT);
     Serial.begin(9600);
-    n = 7;
     pinMode(in1Pin, OUTPUT);
     pinMode(in2Pin, OUTPUT);
+    pinMode(int3Pin, OUTPUT);
+    pinMode(int4Pin, OUTPUT);
     pinMode(enablePin, OUTPUT);
+    pinMode(enablePin2, OUTPUT);
 }
 
 void loop() {
     delay(50);
-    if(Serial.available()){
-        c = (char) Serial.read();
-        if(c == '2'){
-            Serial.println("DUPA");
-        }
-    }
+//    if(Serial.available()){
+//        c = (char) Serial.read();
+//        if(c == '2'){
+//            Serial.println("DUPA");
+//        }
+//    }
 
 //    char c = Serial.read();
 //    data += c;
@@ -48,20 +51,38 @@ void loop() {
 //        }
 //    }
 //BT communication
-//    if (Serial.available()) {
-//        c = Serial.read();
-//        Serial.println(c);
-//    }
+    if (Serial.available()) {
+        c = Serial.read();
+        if(strcmp(c,"D")){
+            setMotorA(speed,reverse);
+            setMotorB(0,reverse);
+        } else if(strcmp(c,"C")){
+            setMotorA(0,reverse);
+            setMotorB(speed,reverse);
+        } else if(strcmp(c,"A")){
+            setMotorA(speed,reverse);
+            setMotorB(speed,reverse);
+        } else if(strcmp(c,"E")){
+            setMotorA(speed,!reverse);
+            setMotorB(speed,!reverse);
+        }
+        Serial.println(c);
+    }
 
 //STEROWANIE SILNIKIEM
-//    int speed = 255;
-//    boolean reverse = true;
 //    setMotor(speed, reverse);
 }
 
-void setMotor(int speed, boolean reverse)
+void setMotorA(int speed, boolean reverse)
 {
     analogWrite(enablePin, speed);
     digitalWrite(in1Pin, reverse);
     digitalWrite(in2Pin, !reverse);
+}
+
+void setMotorB(int speed, boolean reverse)
+{
+    analogWrite(enablePin2, speed);
+    digitalWrite(in3Pin, reverse);
+    digitalWrite(in4Pin, !reverse);
 }
